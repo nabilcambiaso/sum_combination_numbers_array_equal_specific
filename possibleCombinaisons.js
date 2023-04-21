3cfbf101485ee7f8028dc4abcfcdc9d10d5956ad
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-04-21 15:30:44 +0000

    Quick fix to duplicates

--- possibleCombinaisons.js
+++ possibleCombinaisons.js
@@ -1,4 +1,35 @@
+function parseIntArray(arr) {
+  const result = [];
+  for (const elem of arr) {
+    if (Array.isArray(elem)) {
+      result.push(parseIntArray(elem));
+    } else {
+      result.push(parseInt(elem));
+    }
+  }
+  return result;
+}
+
+function addIncrementalDuplicates(arr) {
+  const map = new Map();
+  const result = [];
+
+  for (const elem of arr) {
+    const count = map.get(elem) || 0;
+    map.set(elem, count + 1);
+
+    if (count > 0) {
+      result.push(`${elem}.${count}`);
+    } else {
+      result.push(elem);
+    }
+  }
+
+  return result;
+}
+
 function combinaisons(arr, theNumber) {
+  arr = addIncrementalDuplicates(arr);
   const result = [];
   const possibleCombinaison = [];
   let i = 0;
@@ -10,20 +41,19 @@ function combinaisons(arr, theNumber) {
     }
     if (sum >= theNumber || i === arr.length) {
       const last = possibleCombinaison.pop();
-      sum = sum - last;
+      sum = sum - parseInt(last);
       i = arr.indexOf(last) + 1
     }else{
       const number = arr[i];
       possibleCombinaison.push(number);
-      sum = sum + number;
+      sum = sum + parseInt(number);
       i++;
     }
   
   } while (i < arr.length || possibleCombinaison.length > 0);
 
-  return result;
+  return parseIntArray(result);
 }
 
-
-const result = combinaisons([42,1, 3, 7, 8, 10, 11, 20], 42);
+const result = combinaisons([42,1,1,1,1,1,1,1,1, 40, 1, 3, 8, 10, 11, 20], 42);
 console.log(result);
